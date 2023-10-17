ea0abec2be67198939cd3e9a240e1c960010b872
GG-Services <85185583+GG-Services@users.noreply.github.com>
2023-10-17 13:27:19 +0100

    new

---
+++ js/google-map/map-active.js
@@ -0,0 +1,44 @@
+var map;
+var latlng = new google.maps.LatLng(36.8576811,10.1901619);
+var stylez = [{
+    featureType: "all",
+    elementType: "all",
+    stylers: [{
+        saturation: -10
+            }]
+        }];
+var mapOptions = {
+    zoom: 15,
+    center: latlng,
+    scrollwheel: false,
+    scaleControl: false,
+    disableDefaultUI: true,
+    mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gMap']
+    }
+};
+map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+var marqueur = new google.maps.Marker({
+position: new google.maps.LatLng(36.8576811,10.1901619),
+map: map 
+});
+//var geocoder_map = new google.maps.Geocoder();
+//var address = 'New york';
+//geocoder_map.geocode({
+//    'address': address
+//}, function (results, status) {
+ //   if (status == google.maps.GeocoderStatus.OK) {
+ //       map.setCenter(results[0].geometry.location);
+ //       var marker = new google.maps.Marker({
+//            map: map,
+ //           position: map.getCenter()
+//       });
+//    } else {
+//        alert("Geocode was not successful for the following reason: " + status);
+//    }
+//});
+//var mapType = new google.maps.StyledMapType(stylez, {
+//    name: "Grayscale"
+//});
+//map.mapTypes.set('gMap', mapType);
+//map.setMapTypeId('gMap');
